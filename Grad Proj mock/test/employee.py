23403626f383b626c7a3925e48d8108918ddcd34
utku-helvaci <utku.helvaci.tux@gmail.com>
2023-03-26 15:14:00 +0300

    made the other files

--- Grad Proj mock/test/employee.py
+++ Grad Proj mock/test/employee.py
@@ -34,3 +34,25 @@ con.close()
             finally:
                 return render_template("success.html",msg = msg)
                 con.close()
+
+    @app.route("/deleterecord",methods = ["POST"])
+    def deleterecord():
+        id = request.form["id"]
+        with sqlite3.connect("employee.db") as con:
+            try:
+                cur = con.cursor()
+                cur.execute("delete from Employees where id = ?",id)
+                msg = "record successfully deleted"
+            except:
+                msg = "can't be deleted"
+            finally:
+                return render_template("delete_record.html",msg = msg)
+
+            app.route("/view")
+def view():
+    con = sqlite3.connect("employee.db")
+    con.row_factory = sqlite3.Row
+    cur = con.cursor()
+    cur.execute("select * from Employees")
+    rows = cur.fetchall()
+    return render_template("view.html",rows = rows)
