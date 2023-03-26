550c74e05a7a79919d4359c460ea31f36edbdd3f
utku-helvaci <utku.helvaci.tux@gmail.com>
2023-03-26 15:09:24 +0300

    aaa

--- Grad Proj mock/test/employee.py
+++ Grad Proj mock/test/employee.py
@@ -1,8 +1,5 @@
 import sqlite3  
 
-    @app.route("/")
-    def index():
-        return render_template("index.html");
   
 con = sqlite3.connect("employee.db")  
 print("Database opened successfully")  
@@ -12,3 +9,28 @@ con.execute("create table Employees (id INTEGER PRIMARY KEY AUTOINCREMENT, name 
 print("Table created successfully")  
   
 con.close() 
+
+
+    @app.route("/")
+    def index():
+        return render_template("index.html");
+
+    @app.route("/savedetails",methods = ["POST","GET"])
+    def saveDetails():
+        msg = "msg"
+        if request.method == "POST":
+            try:
+                name = request.form["name"]
+                email = request.form["email"]
+                address = request.form["address"]
+                with sqlite3.connect("employee.db") as con:
+                    cur = con.cursor()
+                    cur.execute("INSERT into Employees (name, email, address) values (?,?,?)",(name,email,address))
+                    con.commit()
+                    msg = "Employee successfully Added"
+            except:
+                con.rollback()
+                msg = "We can not add the employee to the list"
+            finally:
+                return render_template("success.html",msg = msg)
+                con.close()
